0246ac7e327ca0ebb89f16624e46ecba47bc4afd
Pauline Rugwevera <prugwevera28@gmail.com>
2022-02-28 18:23:53 +0000

    Create a file,add logo and intro message

---
+++ constants.py
@@ -0,0 +1,33 @@
+LOGO = (
+    """
+    WELCOME TO THE 
+                                                              
+/  ___|            | |        (_)              | |    (_)| |(_)| |         
+\ `--.  _   _  ___ | |_  __ _  _  _ __    __ _ | |__   _ | | _ | |_  _   _ 
+ `--. \| | | |/ __|| __|/ _` || || '_ \  / _` || '_ \ | || || || __|| | | |
+/\__/ /| |_| |\__ \| |_| (_| || || | | || (_| || |_) || || || || |_ | |_| |
+\____/  \__,_||___/ \__|\__,_||_||_| |_| \__,_||_.__/ |_||_||_| \__| \__, |
+                                                                      __/ |
+                                                                     |___/ 
+  _____         _                                                          
+ |  _  |       (_)                                                         
+ | | | | _   _  _  ____                                                    
+ | | | || | | || ||_  /                                                    
+ \ \/' /| |_| || | / /                                                     
+  \_/\_\ \__,_||_|/___|     
+
+
+
+    """
+)
+
+INTRO_MESSAGE = (
+  "Sustainable living is firmly ingrained "
+  "in our day-to-day conversations, "
+  "yet many of us use buzz words such as biomass "
+  "carbon neutral and climate change despite "
+  "having no idea what they mean. "
+
+
+
+)
