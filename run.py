1e8f1bacb589472af3ad1a9a8f8b17ad456cc211
Pauline Rugwevera <prugwevera28@gmail.com>
2022-03-02 10:20:11 +0000

    Write code to accept and validate user name

--- run.py
+++ run.py
@@ -1,2 +1,22 @@
+import time
 from constants import (LOGO, INTRO_MESSAGE)
-print(LOGO, INTRO_MESSAGE)
+
+def user_name():
+    """
+    Function to accept and validate user name
+    """
+    name_entry = input("Please enter your name: -(Minimum 3 characters)\n")
+    
+    if len(name_entry) < 3:
+        print("Enter at least 3 letters")
+        return user_name()
+    else:
+        print(f"\nWelcome to the Sustainability Quiz {name_entry}\n")
+
+
+def start_game():
+    print(LOGO)
+    time.sleep(1)
+    user_name()
+
+start_game()
