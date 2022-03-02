64831f4e391b74dd381272d1bfa33f64c9c157da
Pauline Rugwevera <prugwevera28@gmail.com>
2022-03-02 11:42:52 +0000

    Write code to create questios array and question object

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import time
 from constants import (LOGO, INTRO_MESSAGE)
+from question import Question
 
 def user_name():
     """
@@ -14,11 +15,34 @@ def user_name():
         print(f"\nWelcome to the Sustainability Quiz {name_entry}\n")
 
 
+questions_prompt = [
+    "1: What is regenerative farming?\n"
+    "(A) A farming method that helps farmers decrease costs\n"
+    "(B) new farming method seen on Doctor Who\n "
+    "(C) A sustainable approach to farminng that locks carbon in the soil\n\n",
+    "What color is strawberry?\n(a) purple\n (b) Green\n (c) Red\n\n"
+    
+    
+    
+    ]
+
+
+questions =[
+    Question(questions_prompt[0], "A"),
+    Question(questions_prompt[1], "C")
+    
+    ]
+
+
+
 def start_game():
     print(LOGO)
     time.sleep(1)
     user_name()
     time.sleep(1)
     print(INTRO_MESSAGE)
+    time.sleep(1)
+    # run_test(questions)
+    time.sleep(1)
 
 start_game()
